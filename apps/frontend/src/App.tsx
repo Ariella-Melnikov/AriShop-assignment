9a283e712da6cc62d26914290678e50d98437b3c
Ariella Melnikov <melnikov.ariella@gmail.com>
2025-03-19 19:46:03 +0200

    add MainPage and MainHero

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,18 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AppHeader } from './components/AppHeader/AppHeader';
+import { MainPage } from "./pages/MainPage";
 
 function App() {
   return (
     <Router>
       <div className="app">
         <AppHeader />
-        <main className="main-content">
+        <main >
           <Routes>
             <Route path="/best-seller" element={<div>Best Seller Page</div>} />
             <Route path="/shop" element={<div>Shop Page</div>} />
             <Route path="/philosophy" element={<div>Philosophy Page</div>} />
             <Route path="/contact" element={<div>Contact Page</div>} />
-            <Route path="/" element={<div>Home Page</div>} />
+            <Route path="/" element={<MainPage />} />
           </Routes>
         </main>
       </div>
---
+++ apps/frontend/src/pages/MainPage.tsx
@@ -0,0 +1,12 @@
+import { MainHero } from '../components/Hero/MainHero'
+
+export const MainPage = () => {
+    return (
+        <main className='main-page'>
+            <section className='hero-section'>
+                <MainHero />
+            </section>
+        </main>
+    )
+}
+
